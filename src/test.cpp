0530de74f4fa8e944338dfaffdb4d97e5c113a28
BinFench <benf@DESKTOP-F27627H.localdomain>
2020-10-14 20:06:36 -0300

    more tests

--- src/test.cpp
+++ src/test.cpp
@@ -15,8 +15,44 @@ int main()
         std::cout << "Passed" << std::endl;
         passed++;
     } else {
-        std::cout << "Failed 1" << std::endl;
+        std::cout << "Failed 2" << std::endl;
     }
-    std::cout << passed << "/" << 2 << " cases pass" << std::endl;
+    if (newParser->parse("pass", newParser->any())) {
+        std::cout << "Passed" << std::endl;
+        passed++;
+    } else {
+        std::cout << "Failed 3" << std::endl;
+    }
+    if (!newParser->parse("", newParser->any())) {
+        std::cout << "Passed" << std::endl;
+        passed++;
+    } else {
+        std::cout << "Failed 4" << std::endl;
+    }
+    if (newParser->parse("pass", newParser->anyOf(newParser->String("pass")))) {
+        std::cout << "Passed" << std::endl;
+        passed++;
+    } else {
+        std::cout << "Failed 5" << std::endl;
+    }
+    if (!newParser->parse("fail", newParser->anyOf(newParser->String("pass"), newParser->String("succeed")))) {
+        std::cout << "Passed" << std::endl;
+        passed++;
+    } else {
+        std::cout << "Failed 6" << std::endl;
+    }
+    if (newParser->parse("pass", newParser->anyOf(newParser->String("fail"), newParser->String("pass")))) {
+        std::cout << "Passed" << std::endl;
+        passed++;
+    } else {
+        std::cout << "Failed 7" << std::endl;
+    }
+    if (!newParser->parse("pas", newParser->anyOf(newParser->String("fail"), newParser->String("pass")))) {
+        std::cout << "Passed" << std::endl;
+        passed++;
+    } else {
+        std::cout << "Failed 8" << std::endl;
+    }
+    std::cout << passed << "/" << 8 << " cases pass" << std::endl;
     return 0;
 }
